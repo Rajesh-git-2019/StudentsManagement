273c6474a21f2a60aa40ae5979703ab143e8d0bf
unknown <rajeshgithub2019@gmail.com>
2020-04-05 18:00:37 -0400

    Student Profile Functionality

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-light bg-dark" >Welcome to Student Managemnet System !!!</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                    <br>
+                    <br>
+                    <button class="navbar-brand btn-outline-success" href="{{ url('/') }}">
+                    View Students Profile
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/students/layout.blade.php
@@ -0,0 +1,12 @@
+
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="card" style="margin-top: 20px;">
+        <div class="card-body">
+            @yield('content')
+        </div>
+    </div>
+</div>
+@endsection
+
